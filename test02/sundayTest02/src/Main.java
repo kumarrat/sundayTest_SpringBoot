07c064b753c511b4481f2b2d18d28d815684add6
Kumar Ratnesh <rockratnsh@gmail.com>
2023-09-03 15:20:28 +0530

    call API using java

---
+++ test02/sundayTest02/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        var url = "https://api.nationalize.io/?name=nathaniel";
+
+        var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+        var client = HttpClient.newBuilder().build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+    }
+}
